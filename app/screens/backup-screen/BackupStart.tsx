16ed38beb46e31e722ad3a9ecb7538078e1c5705
nodirbek75 <nodirbek7077@gmail.com>
2024-01-26 19:28:12 +0500

    Empty BackupStart screen added

---
+++ app/screens/backup-screen/BackupStart.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import styled from "styled-components/native"
+
+const BackupStart = () => {
+  return <Wrapper />
+}
+
+export default BackupStart
+
+const Wrapper = styled.View`
+  flex: 1;
+  background-color: yellowgreen;
+`
---
+++ app/screens/backup-screen/index.ts
@@ -0,0 +1,3 @@
+import BackupStart from "./BackupStart"
+
+export { BackupStart }
