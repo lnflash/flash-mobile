0fc32420b8b529db58496127b7d9b11332464686
nodirbek75 <nodirbek7077@gmail.com>
2024-02-02 21:35:13 +0500

    BackupShowSeedPhrase screen added

--- app/screens/backup-screen/index.ts
+++ app/screens/backup-screen/index.ts
@@ -3,5 +3,13 @@ import BackupSeedPhrase from "./BackupSeedPhrase"
 import BackupDoubleCheck from "./BackupDoubleCheck"
 import BackupVerify from "./BackupVerify"
 import BackupComplete from "./BackupComplete"
+import BackupShowSeedPhrase from "./BackupShowSeedPhrase"
 
-export { BackupStart, BackupSeedPhrase, BackupDoubleCheck, BackupVerify, BackupComplete }
+export {
+  BackupStart,
+  BackupSeedPhrase,
+  BackupDoubleCheck,
+  BackupVerify,
+  BackupComplete,
+  BackupShowSeedPhrase,
+}
