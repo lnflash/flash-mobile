99fa04f111b7123639066e320375f71c405cf608
nodirbek75 <nodirbek7077@gmail.com>
2024-04-27 14:01:05 +0500

    fix text not showing in dark mode issue

--- app/screens/transaction-history/USDTransactionHistory.tsx
+++ app/screens/transaction-history/USDTransactionHistory.tsx
@@ -1,8 +1,8 @@
 import * as React from "react"
 import crashlytics from "@react-native-firebase/crashlytics"
-import { ActivityIndicator, SectionList, Text, View } from "react-native"
+import { ActivityIndicator, SectionList, View } from "react-native"
 import { useI18nContext } from "@app/i18n/i18n-react"
-import { makeStyles, useTheme } from "@rneui/themed"
+import { Text, makeStyles, useTheme } from "@rneui/themed"
 
 // components
 import { Screen } from "@app/components/screen"
