831f24e2b68118ac65c71c0485cbe254ca60e44e
abhay-raizada <toabhayraizada@gmail.com>
2024-07-02 16:10:52 +0530

    Use Flash Relay

--- app/navigation/root-navigator.tsx
+++ app/navigation/root-navigator.tsx
@@ -656,7 +656,7 @@ export const PrimaryNavigator = () => {
           ),
         }}
       /> */}
-      {/* <Tab.Screen
+      <Tab.Screen
         name="Chat"
         component={ChatNavigator}
         options={{
@@ -664,7 +664,7 @@ export const PrimaryNavigator = () => {
           title: LL.ChatScreen.title(),
           tabBarIcon: ({ color }) => <ChatIcon color={color} />,
         }}
-      /> */}
+      />
       <Tab.Screen
         name="Card"
         component={CardScreen}
