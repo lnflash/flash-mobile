a3d212e46988ca7afa0c63ba526ed50a16a54aa9
abhay-raizada <toabhayraizada@gmail.com>
2024-07-11 16:37:02 +0530

    Add A different NIP17 Component

--- app/screens/chat-screen/chat-screen.types.d.ts
+++ app/screens/chat-screen/chat-screen.types.d.ts
@@ -1,11 +1,17 @@
 type Chat = {
   readonly id: string
-  readonly name: string
-  readonly username: string
+  readonly name?: string
+  readonly username?: string
   readonly alias?: string | null
-  lud16: string
-  picture: string | null
-  readonly transactionsCount: number | null
+  lud16?: string
+  picture?: string | null
+  readonly transactionsCount?: number | null
+}
+
+type NIP17Chat = {
+  id: string
+  subject: string
+  participants: string[]
 }
 
 type NostrProfile = {
