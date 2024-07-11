dd972c584c0042c6e3a0bf7f069036dbd3184861
nodirbek75 <nodirbek7077@gmail.com>
2024-07-11 15:06:08 +0500

    notification settings screen added

--- app/navigation/root-navigator.tsx
+++ app/navigation/root-navigator.tsx
@@ -99,6 +99,7 @@ import {
 } from "@app/screens"
 import { useAppSelector } from "@app/store/redux"
 import { usePersistentStateContext } from "@app/store/persistent-state"
+import { NotificationSettingsScreen } from "@app/screens/settings-screen/notifications-screen"
 
 const useStyles = makeStyles(({ colors }) => ({
   bottomNavigatorStyle: {
@@ -393,6 +394,13 @@ export const RootStack = () => {
         component={AccountScreen}
         options={{
           title: LL.common.account(),
+        }}
+      />
+      <RootNavigator.Screen
+        name="notificationSettingsScreen"
+        component={NotificationSettingsScreen}
+        options={{
+          title: LL.NotificationSettingsScreen.title(),
         }}
       />
       <RootNavigator.Screen
