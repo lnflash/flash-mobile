8daa31f4dcf2bedd7d2ddbc9798ca8ef58e73c84
abhay-raizada <toabhayraizada@gmail.com>
2024-07-11 17:47:46 +0530

    Add Messages Screen

---
+++ app/screens/nip17-chat/messages.tsx
@@ -0,0 +1,200 @@
+import "react-native-get-random-values"
+import * as React from "react"
+import { ActivityIndicator, Image, View } from "react-native"
+import { useI18nContext } from "@app/i18n/i18n-react"
+import { RouteProp, useNavigation } from "@react-navigation/native"
+import { StackNavigationProp } from "@react-navigation/stack"
+import { Screen } from "../../components/screen"
+import type {
+  ChatStackParamList,
+  RootStackParamList,
+} from "../../navigation/stack-param-lists"
+import { makeStyles, Text, useTheme } from "@rneui/themed"
+import { GaloyIconButton } from "@app/components/atomic/galoy-icon-button"
+import { isIos } from "@app/utils/helper"
+import { Chat, MessageType, defaultTheme } from "@flyerhq/react-native-chat-ui"
+import { launchImageLibrary } from "react-native-image-picker"
+import { ChatMessage } from "./chatMessage"
+import useNostrProfile from "@app/hooks/use-nostr-profile"
+import Icon from "react-native-vector-icons/Ionicons"
+import { Event } from "nostr-tools"
+import { Rumor } from "@app/utils/nostr"
+
+type MessagesProps = {
+  route: RouteProp<ChatStackParamList, "messages">
+}
+
+export const Messages: React.FC<MessagesProps> = ({ route }) => {
+  const { participants, rumors, userPubkey } = route.params
+  return (
+    <MessagesScreen userPubkey={userPubkey} participants={participants} rumors={rumors} />
+  )
+}
+
+type MessagesScreenProps = {
+  userPubkey: string
+  participants: string[]
+  rumors: Rumor[]
+}
+
+export const MessagesScreen: React.FC<MessagesScreenProps> = ({
+  userPubkey,
+  participants,
+  rumors,
+}) => {
+  const {
+    theme: { colors },
+  } = useTheme()
+  const styles = useStyles()
+  const navigation = useNavigation<StackNavigationProp<RootStackParamList, "Primary">>()
+  const { LL } = useI18nContext()
+  const [initialized, setInitialized] = React.useState(false)
+
+  const convertRumorsToMessages = (rumors: Rumor[]): MessageType.Text[] => {
+    return rumors.map((r) => {
+      return {
+        author: user,
+        createdAt: r.created_at,
+        id: r.id,
+        type: "text",
+        text: r.content,
+      }
+    })
+  }
+
+  React.useEffect(() => {
+    let isMounted = true
+    async function initialize() {
+      setInitialized(true)
+    }
+    if (!initialized) initialize()
+
+    return () => {
+      isMounted = false
+    }
+  }, [])
+
+  const user = { id: userPubkey }
+
+  const handleSendPress = async (message: MessageType.PartialText) => {
+    const textMessage: MessageType.Text = {
+      author: user,
+      createdAt: Date.now(),
+      id: user.id,
+      text: message.text,
+      type: "text",
+    }
+    //await sendNip17Message(chat.id, message.text)
+  }
+
+  return (
+    <Screen unsafe>
+      <View style={styles.aliasView}>
+        <Icon
+          name="arrow-back-outline"
+          onPress={navigation.goBack}
+          style={styles.backButton}
+        />
+        <Text type="p1">{userPubkey}</Text>
+        <View style={{ display: "flex", flexDirection: "row" }}>
+          <GaloyIconButton
+            name={"lightning"}
+            size="large"
+            //text={LL.HomeScreen.pay()}
+            style={{ marginRight: 5 }}
+            onPress={() =>
+              navigation.navigate("sendBitcoinDestination", {
+                //username: chat.lud16,
+              })
+            }
+          />
+          <Image
+            source={{
+              uri: "https://pfp.nostr.build/520649f789e06c2a3912765c0081584951e91e3b5f3366d2ae08501162a5083b.jpg",
+            }}
+            style={styles.userPic}
+          />
+        </View>
+      </View>
+      {!initialized && <ActivityIndicator />}
+      <View style={styles.chatBodyContainer}>
+        <View style={styles.chatView}>
+          <Chat
+            messages={convertRumorsToMessages(rumors)}
+            onPreviewDataFetched={() => {}}
+            onSendPress={handleSendPress}
+            l10nOverride={{
+              emptyChatPlaceholder: initialized
+                ? isIos
+                  ? "No messages here yet"
+                  : "..."
+                : isIos
+                ? "Fetching Messages..."
+                : "...",
+            }}
+            showUserAvatars={true}
+            user={user}
+            renderTextMessage={(message, nextMessage, prevMessage) => (
+              <ChatMessage
+                message={message}
+                recipientId={userPubkey}
+                nextMessage={nextMessage}
+                prevMessage={prevMessage}
+              />
+            )}
+            theme={{
+              ...defaultTheme,
+              colors: {
+                ...defaultTheme.colors,
+                inputBackground: colors._black,
+                background: colors._lightGrey,
+              },
+              fonts: {
+                ...defaultTheme.fonts,
+                sentMessageBodyTextStyle: {
+                  ...defaultTheme.fonts.sentMessageBodyTextStyle,
+                  fontSize: 12,
+                },
+              },
+            }}
+          />
+        </View>
+      </View>
+    </Screen>
+  )
+}
+
+const useStyles = makeStyles(({ colors }) => ({
+  actionsContainer: {
+    margin: 12,
+  },
+  aliasView: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingRight: 10,
+    paddingLeft: 10,
+    paddingBottom: 6,
+    paddingTop: isIos ? 40 : 10,
+  },
+  chatBodyContainer: {
+    flex: 1,
+  },
+  chatView: {
+    flex: 1,
+    marginHorizontal: 30,
+    borderRadius: 24,
+    overflow: "hidden",
+  },
+  userPic: {
+    borderRadius: 50,
+    height: 50,
+    width: 50,
+    borderWidth: 1,
+    borderColor: colors.green,
+  },
+  backButton: {
+    fontSize: 26,
+  },
+}))
