36afa199e5c381559d963832f0b4a0ba7db2ead5
abhay-raizada <toabhayraizada@gmail.com>
2024-07-18 17:12:29 +0530

    Fix Sends

--- app/screens/nip17-chat/historyListItem.tsx
+++ app/screens/nip17-chat/historyListItem.tsx
@@ -1,54 +1,76 @@
 import { ListItem, useTheme } from "@rneui/themed"
 import { useStyles } from "./style"
 import { Image } from "react-native"
-import { UnsignedEvent, nip19 } from "nostr-tools"
-import { Rumor } from "@app/utils/nostr"
+import { UnsignedEvent, nip19, Event } from "nostr-tools"
+import { fetchNostrUsers } from "@app/utils/nostr"
 import { useNavigation } from "@react-navigation/native"
 import { StackNavigationProp } from "@react-navigation/stack"
 import { ChatStackParamList } from "@app/navigation/stack-param-lists"
+import { useEffect, useState } from "react"
 
 interface HistoryListItemProps {
-  item: Rumor[]
-  userPubKey: string
+  item: string
+  userPrivateKey: Uint8Array
 }
-export const HistoryListItem: React.FC<HistoryListItemProps> = ({ item, userPubKey }) => {
+export const HistoryListItem: React.FC<HistoryListItemProps> = ({
+  item,
+  userPrivateKey,
+}) => {
+  const [profileMap, setProfileMap] = useState<Map<string, Object>>()
+  useEffect(() => {
+    fetchNostrUsers(item.split(",")).then((profiles: Event[]) => {
+      let profilesMap = new Map<string, Object>()
+      profiles.forEach((profile) => {
+        try {
+          let content = JSON.parse(profile.content)
+          profilesMap.set(profile.pubkey, content)
+        } catch (e) {
+          console.log("error parsing profile", profile.content)
+          return
+        }
+      })
+      setProfileMap(profilesMap)
+    })
+  }, [item])
   const styles = useStyles()
-  let participants = item[0].tags.filter((t) => t[0] === "p").map((p) => p[1])
-  const participantsSet = new Set([...participants, item[0].pubkey])
-  participants = Array.from(participantsSet)
-  participants.sort()
-  const id = participants.join(",")
-  const displayParticipants = participants.map((p) => nip19.npubEncode(p))
-  const displayString = displayParticipants.join(", ")
-  const subject = item[0].tags.find((t) => t[0] === "subject")?.[1]
   const {
     theme: { colors },
   } = useTheme()
   const navigation = useNavigation<StackNavigationProp<ChatStackParamList, "chatList">>()
   return (
     <ListItem
-      key={id}
+      key={item}
       style={styles.item}
       containerStyle={styles.itemContainer}
       onPress={() =>
         navigation.navigate("messages", {
-          userPubkey: userPubKey,
-          participants,
-          rumors: item,
+          groupId: item,
+          userPrivateKey,
         })
       }
     >
-      {/* <Image
-        source={{
-          uri:
-            item. ||
-            "https://pfp.nostr.build/520649f789e06c2a3912765c0081584951e91e3b5f3366d2ae08501162a5083b.jpg",
-        }}
-        style={styles.profilePicture}
-      /> */}
+      {Array.from(profileMap?.values() || []).map((profile: any) => {
+        return (
+          <Image
+            source={{
+              uri:
+                profile.picture ||
+                "https://pfp.nostr.build/520649f789e06c2a3912765c0081584951e91e3b5f3366d2ae08501162a5083b.jpg",
+            }}
+            style={styles.profilePicture}
+          />
+        )
+      })}
       <ListItem.Content>
         <ListItem.Title style={styles.itemText}>
-          {subject || displayString}
+          <>
+            {item
+              .split(",")
+              .map((pubkey) => {
+                return (profileMap?.get(pubkey) as any)?.lud16 || pubkey
+              })
+              .join(", ")}
+          </>
         </ListItem.Title>
       </ListItem.Content>
     </ListItem>
