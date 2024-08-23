429b63295a13079737a13c0e673655a833e060a8
nodirbek75 <nodirbek7077@gmail.com>
2024-08-23 18:26:21 +0500

    send-bitcoin-confirmation-screen subcomponents are added

--- app/components/send-flow/index.ts
+++ app/components/send-flow/index.ts
@@ -2,5 +2,18 @@ import DestinationField from "./DestinationField"
 import ChooseWallet from "./ChooseWallet"
 import DetailDestination from "./DetailDestination"
 import DetailAmountNote from "./DetailAmountNote"
+import SendingAnimation from "./SendingAnimation"
+import ConfirmationDestinationAmountNote from "./ConfirmationDestinationAmountNote"
+import ConfirmationWalletFee from "./ConfirmationWalletFee"
+import ConfirmationError from "./ConfirmationError"
 
-export { DestinationField, ChooseWallet, DetailDestination, DetailAmountNote }
+export {
+  DestinationField,
+  ChooseWallet,
+  DetailDestination,
+  DetailAmountNote,
+  SendingAnimation,
+  ConfirmationDestinationAmountNote,
+  ConfirmationWalletFee,
+  ConfirmationError,
+}
