ce964a1323d5ca559a9504b66faf1d7c873f57cc
nodirbek75 <nodirbek7077@gmail.com>
2024-08-29 19:49:39 +0500

    Added subcomponents for redeem-bitcoin-result-screen

--- app/components/redeem-flow/index.ts
+++ app/components/redeem-flow/index.ts
@@ -1,4 +1,6 @@
 import DetailDescription from "./DetailDescription"
 import InforError from "./InforError"
+import ResultError from "./ResultError"
+import ResultSuccess from "./ResultSuccess"
 
-export { DetailDescription, InforError }
+export { DetailDescription, InforError, ResultError, ResultSuccess }
