9466c4dbf30323bec543bdb5c8eacf64d6e65cd5
nodirbek75 <nodirbek7077@gmail.com>
2024-09-05 16:44:34 +0500

    remove unused styles in date-range-display

--- app/components/date-range-display/date-range-display.tsx
+++ app/components/date-range-display/date-range-display.tsx
@@ -2,6 +2,8 @@ import React from "react"
 import { View, Text } from "react-native"
 import { useI18nContext } from "@app/i18n/i18n-react"
 import { makeStyles } from "@rneui/themed"
+
+// utils
 import { formatDate } from "@app/utils/transaction-filters"
 
 export const DateRangeDisplay: React.FC<{ from: string; to: string }> = ({
@@ -26,59 +28,6 @@ export const DateRangeDisplay: React.FC<{ from: string; to: string }> = ({
 }
 
 const useStyles = makeStyles(({ colors }) => ({
-  container: {
-    flex: 1,
-    padding: 16,
-    backgroundColor: colors.background,
-  },
-  filterContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    marginBottom: 16,
-  },
-  totalText: {
-    fontSize: 24,
-    fontWeight: "bold",
-    marginBottom: 16,
-    textAlign: "center",
-    color: colors.black,
-  },
-  scrollView: {
-    marginBottom: 16,
-  },
-  transactionRow: {
-    paddingVertical: 8,
-    borderTopWidth: 2,
-    borderTopColor: colors.grey4,
-  },
-  transactionDetailsRow: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-  },
-  dateText: {
-    fontSize: 14,
-    color: colors.black,
-    flex: 1,
-  },
-  txDirectionText: {
-    fontSize: 14,
-    color: colors.black,
-    textAlign: "center",
-    flex: 1,
-  },
-  amountText: {
-    fontSize: 14,
-    color: colors.black,
-    textAlign: "right",
-    flex: 1,
-  },
-  sectionTitle: {
-    fontSize: 20,
-    fontWeight: "bold",
-    marginVertical: 8,
-    color: colors.primary,
-  },
   dateContainer: {
     flexDirection: "row",
     justifyContent: "space-between",
@@ -102,11 +51,5 @@ const useStyles = makeStyles(({ colors }) => ({
   dateValue: {
     fontSize: 12,
     color: colors.black,
-  },
-  button: {
-    marginVertical: 10,
-    // center the button with space on the sides
-    marginHorizontal: "auto",
-    paddingHorizontal: 40,
   },
 }))
