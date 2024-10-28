60f851d90c7fe8a15a271c8afdae021eca993b7f
nodirbek75 <nodirbek7077@gmail.com>
2024-10-28 13:16:49 +0500

    useCashout hook is added

---
+++ app/hooks/useCashout.ts
@@ -0,0 +1,38 @@
+import {
+  HomeAuthedDocument,
+  useAccountDefaultWalletLazyQuery,
+  useIntraLedgerUsdPaymentSendMutation,
+} from "@app/graphql/generated"
+
+export const useCashout = () => {
+  const [intraLedgerUsdPaymentSend] = useIntraLedgerUsdPaymentSendMutation({
+    refetchQueries: [HomeAuthedDocument],
+  })
+
+  const [accountDefaultWalletQuery] = useAccountDefaultWalletLazyQuery({
+    fetchPolicy: "no-cache",
+  })
+
+  const onCashout = async (walletId: string, amount: number) => {
+    const { data } = await accountDefaultWalletQuery({
+      variables: { username: process.env.CASHOUT_ADDRESS as string },
+    })
+
+    if (data) {
+      const res = await intraLedgerUsdPaymentSend({
+        variables: {
+          input: {
+            walletId,
+            recipientWalletId: data?.accountDefaultWallet.id,
+            amount,
+            memo: "",
+          },
+        },
+      })
+
+      return res.data?.intraLedgerUsdPaymentSend
+    }
+  }
+
+  return { onCashout }
+}
