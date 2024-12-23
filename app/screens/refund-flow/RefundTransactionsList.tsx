04de912ddcfac9e747acc4d11ca3bbe2daf99d68
nodirbek75 <nodirbek7077@gmail.com>
2024-12-23 22:45:43 +0500

    fix 'Cannot convert null value to object' issue

--- app/screens/refund-flow/RefundTransactionsList.tsx
+++ app/screens/refund-flow/RefundTransactionsList.tsx
@@ -44,8 +44,8 @@ const RefundTransactionsList: React.FC<Props> = ({ navigation }) => {
 
   const fetchRefundables = async () => {
     setLoading(true)
-    const refundables = await listRefundables()
-    const refundedTxs = await loadJson("refundedTxs")
+    const refundables = (await listRefundables()) || []
+    const refundedTxs = (await loadJson("refundedTxs")) || []
     const merged = mergeByTimestamp(refundables, refundedTxs)
     setRefundables(merged)
     setLoading(false)
