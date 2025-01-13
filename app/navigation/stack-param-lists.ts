f6de5006db76177978fce5127f32b0c5dd8d11f8
nodirbek75 <nodirbek7077@gmail.com>
2025-01-13 15:32:25 +0500

    clean up

--- app/navigation/stack-param-lists.ts
+++ app/navigation/stack-param-lists.ts
@@ -14,7 +14,6 @@ import {
 } from "@app/screens/send-bitcoin-screen/payment-destination/index.types"
 import { WalletDescriptor } from "@app/types/wallets"
 import _Map from "@app/assets/icons-redesign/map.svg"
-import { RefundableSwap } from "@breeztech/react-native-breez-sdk-liquid"
 
 export type RootStackParamList = {
   Reconciliation: { from: string; to: string }
