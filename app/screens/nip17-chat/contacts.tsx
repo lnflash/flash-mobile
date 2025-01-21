0e76a9c87e488faac1aa17bc05538567676d82f2
abhay-raizada <toabhayraizada@gmail.com>
2025-01-21 18:32:44 +0530

    Fix Styles

--- app/screens/nip17-chat/contacts.tsx
+++ app/screens/nip17-chat/contacts.tsx
@@ -1,15 +1,17 @@
 // Contacts.tsx
 import React, { useState } from "react"
-import { Alert, FlatList, Text, View } from "react-native"
+import { Alert, FlatList, Text, View, Image } from "react-native"
 import { useStyles } from "./style" // Adjust the path as needed
 import { bytesToHex } from "@noble/hashes/utils"
 import { fetchContactList, fetchSecretFromLocalStorage } from "@app/utils/nostr"
 import { useChatContext } from "./chatContext"
 import { getPublicKey, nip19 } from "nostr-tools"
 import { useFocusEffect, useNavigation } from "@react-navigation/native"
-import { ListItem } from "@rneui/themed"
+import { ListItem, useTheme } from "@rneui/themed"
 import { StackNavigationProp } from "@react-navigation/stack"
 import { ChatStackParamList } from "@app/navigation/stack-param-lists"
+import ChatIcon from "@app/assets/icons/chat.svg"
+import { GaloyIconButton } from "@app/components/atomic/galoy-icon-button"
 
 interface ContactsProps {
   userPrivateKey: string
@@ -19,8 +21,10 @@ const Contacts: React.FC<ContactsProps> = ({ userPrivateKey }) => {
   const styles = useStyles()
   const [contacts, setContacts] = useState<NostrProfile[]>([])
   const [initialized, setInitialized] = useState(false)
-  const { poolRef } = useChatContext()
+  const { poolRef, profileMap, addEventToProfiles } = useChatContext()
   const navigation = useNavigation<StackNavigationProp<ChatStackParamList, "chatList">>()
+  const { theme } = useTheme()
+  const colors = theme.colors
   useFocusEffect(
     React.useCallback(() => {
       async function initialize() {
@@ -46,36 +50,71 @@ const Contacts: React.FC<ContactsProps> = ({ userPrivateKey }) => {
       }
     }, []),
   )
+  const getContactMetadata = (contact: NostrProfile) => {
+    let profile = profileMap?.get(contact.pubkey || "")
+    return (
+      profile?.nip05 ||
+      profile?.name ||
+      profile?.username ||
+      nip19.npubEncode(contact.pubkey!).slice(0, 9) + ".."
+    )
+  }
   return (
     <View>
-      <Text>Contacts: {contacts.join(", ")}</Text>
       <FlatList
         contentContainerStyle={styles.listContainer}
         data={contacts}
         ListEmptyComponent={<Text>No Contacts Available</Text>}
         renderItem={({ item }) => (
-          <ListItem
-            style={styles.item}
-            containerStyle={styles.itemContainer}
-            onPress={() =>
-              navigation.navigate("messages", {
-                groupId: item.pubkey!,
-                userPrivateKey,
-              })
-            }
-          >
-            <ListItem.Content key="heading">
+          <ListItem style={styles.item} containerStyle={styles.itemContainer}>
+            <Image
+              source={{
+                uri:
+                  profileMap?.get(item.pubkey!)?.picture ||
+                  "https://pfp.nostr.build/520649f789e06c2a3912765c0081584951e91e3b5f3366d2ae08501162a5083b.jpg",
+              }}
+              style={styles.profilePicture}
+              key={item.pubkey!}
+            />
+            <ListItem.Content
+              key="heading"
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+              }}
+            >
               <ListItem.Subtitle style={styles.itemText} key="subheading">
                 <Text
                   style={{
-                    color: "white",
+                    color: colors.primary3,
                   }}
                 >
                   {" "}
-                  {item.username || nip19.npubEncode(item.pubkey!)}
+                  {getContactMetadata(item)}
                 </Text>
               </ListItem.Subtitle>
             </ListItem.Content>
+            <ChatIcon
+              color={colors.primary}
+              onPress={() =>
+                navigation.navigate("messages", {
+                  groupId: item.pubkey!,
+                  userPrivateKey,
+                })
+              }
+            />
+            <GaloyIconButton
+              name={"lightning"}
+              size="medium"
+              style={{ margin: 5 }}
+              onPress={() => {
+                navigation.navigate("sendBitcoinDestination", {
+                  username: profileMap?.get(item.pubkey!)?.lud16 || "",
+                })
+              }}
+              key="lightning-button"
+            />
           </ListItem>
         )}
         keyExtractor={(item) => item.pubkey!}
