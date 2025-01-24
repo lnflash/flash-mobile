d167efca7fc4563767686d74e75ca10dee7b3f2f
nodirbek75 <nodirbek7077@gmail.com>
2025-01-24 16:31:12 +0500

    update galoy-error-box component

--- app/components/atomic/galoy-error-box/galoy-error-box.tsx
+++ app/components/atomic/galoy-error-box/galoy-error-box.tsx
@@ -1,23 +1,29 @@
 import { makeStyles, Text, useTheme } from "@rneui/themed"
 import React from "react"
-import { View } from "react-native"
+import { View, ViewStyle } from "react-native"
 import { GaloyIcon } from "../galoy-icon"
 
 type GaloyErrorBoxProps = {
   errorMessage: string
   noIcon?: boolean
+  isWarning?: boolean
+  style?: ViewStyle
 }
 
-export const GaloyErrorBox: React.FC<GaloyErrorBoxProps> = ({ errorMessage, noIcon }) => {
-  const {
-    theme: { colors, mode },
-  } = useTheme()
+export const GaloyErrorBox: React.FC<GaloyErrorBoxProps> = ({
+  errorMessage,
+  noIcon,
+  isWarning,
+  style,
+}) => {
+  const { colors, mode } = useTheme().theme
   const styles = useStyles()
 
-  const color = mode === "light" ? colors.error : colors.black
+  const color =
+    mode === "light" ? (isWarning ? colors.warning : colors.error) : colors.black
 
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, style]}>
       {!noIcon && <GaloyIcon name="warning" size={14} color={color} />}
       <Text style={styles.textContainer} type={"p3"} color={color}>
         {errorMessage}
