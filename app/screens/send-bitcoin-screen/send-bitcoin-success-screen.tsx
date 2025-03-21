ab1b8d9acc6dc886c96482916a81002665c98ab0
nodirbek75 <nodirbek7077@gmail.com>
2025-03-21 12:25:33 +0500

    redesign and clean up send-bitcoin-successs-screen

--- app/screens/send-bitcoin-screen/send-bitcoin-success-screen.tsx
+++ app/screens/send-bitcoin-screen/send-bitcoin-success-screen.tsx
@@ -1,37 +1,64 @@
-import React, { useCallback, useEffect } from "react"
+import React, { useCallback, useEffect, useState } from "react"
+import Rate from "react-native-rate"
+import { View, Alert } from "react-native"
+import { makeStyles, Text, useTheme } from "@rneui/themed"
+import { StackScreenProps } from "@react-navigation/stack"
+import crashlytics from "@react-native-firebase/crashlytics"
+import { RootStackParamList } from "@app/navigation/stack-param-lists"
 
-import { GaloyIcon } from "@app/components/atomic/galoy-icon"
-import { Screen } from "@app/components/screen"
+// hooks
+import { useApolloClient } from "@apollo/client"
+import { useI18nContext } from "@app/i18n/i18n-react"
+import { useSafeAreaInsets } from "react-native-safe-area-context"
+import { useDisplayCurrency, usePriceConversion } from "@app/hooks"
+import { useFeedbackModalShownQuery, WalletCurrency } from "@app/graphql/generated"
+
+// components
 import {
   SuccessIconAnimation,
   SuccessTextAnimation,
 } from "@app/components/success-animation"
-import { useI18nContext } from "@app/i18n/i18n-react"
-import { RootStackParamList } from "@app/navigation/stack-param-lists"
-import { useNavigation } from "@react-navigation/native"
-import { StackNavigationProp } from "@react-navigation/stack"
-import { makeStyles, Text } from "@rneui/themed"
-import { View, Alert } from "react-native"
-import { testProps } from "../../utils/testProps"
-import Rate from "react-native-rate"
-import { ratingOptions } from "@app/config"
-import crashlytics from "@react-native-firebase/crashlytics"
-import { useApolloClient } from "@apollo/client"
-import { useFeedbackModalShownQuery } from "@app/graphql/generated"
-import { setFeedbackModalShown } from "@app/graphql/client-only-query"
+import { Screen } from "@app/components/screen"
+import { PrimaryBtn } from "@app/components/buttons"
 import { SuggestionModal } from "./suggestion-modal"
+import { GaloyIcon } from "@app/components/atomic/galoy-icon"
+
+// utils
+import { ratingOptions } from "@app/config"
+import { testProps } from "../../utils/testProps"
 import { logAppFeedback } from "@app/utils/analytics"
+import { setFeedbackModalShown } from "@app/graphql/client-only-query"
+import { DisplayCurrency, isNonZeroMoneyAmount } from "@app/types/amounts"
 
-const SendBitcoinSuccessScreen = () => {
-  const styles = useStyles()
-  const [showSuggestionModal, setShowSuggestionModal] = React.useState(false)
-  const navigation =
-    useNavigation<StackNavigationProp<RootStackParamList, "sendBitcoinSuccess">>()
+type Props = StackScreenProps<RootStackParamList, "sendBitcoinSuccess">
 
+const SendBitcoinSuccessScreen: React.FC<Props> = ({ navigation, route }) => {
   const client = useApolloClient()
+  const styles = useStyles()
+  const { colors } = useTheme().theme
+  const { bottom } = useSafeAreaInsets()
+  const { LL } = useI18nContext()
+  const { convertMoneyAmount } = usePriceConversion()
+  const { formatMoneyAmount, getSecondaryAmountIfCurrencyIsDifferent } =
+    useDisplayCurrency()
+
+  const [showSuggestionModal, setShowSuggestionModal] = useState(false)
+
   const feedbackShownData = useFeedbackModalShownQuery()
   const feedbackModalShown = feedbackShownData?.data?.feedbackModalShown
-  const { LL } = useI18nContext()
+
+  if (!convertMoneyAmount) return
+
+  useEffect(() => {
+    // if (!feedbackModalShown) {
+    //   const feedbackTimeout = setTimeout(() => {
+    //     requestFeedback()
+    //   }, 3000)
+    //   return () => {
+    //     clearTimeout(feedbackTimeout)
+    //   }
+    // }
+  }, [client, feedbackModalShown, LL])
 
   const dismiss = () => {
     logAppFeedback({
@@ -76,38 +103,78 @@ const SendBitcoinSuccessScreen = () => {
     setFeedbackModalShown(client, true)
   }, [LL, client])
 
-  const FEEDBACK_DELAY = 3000
-  const CALLBACK_DELAY = 5000
-  useEffect(() => {
-    // if (!feedbackModalShown) {
-    //   const feedbackTimeout = setTimeout(() => {
-    //     requestFeedback()
-    //   }, FEEDBACK_DELAY)
-    //   return () => {
-    //     clearTimeout(feedbackTimeout)
-    //   }
-    // }
-    if (!showSuggestionModal) {
-      const navigateToHomeTimeout = setTimeout(
-        () => navigation.popToTop(),
-        CALLBACK_DELAY,
-      )
-      return () => clearTimeout(navigateToHomeTimeout)
-    }
-  }, [client, feedbackModalShown, LL, showSuggestionModal, navigation, requestFeedback])
+  let formattedPrimaryAmount = undefined
+  let formattedSecondaryAmount = undefined
+  const { walletCurrency, unitOfAccountAmount } = route.params
+
+  if (isNonZeroMoneyAmount(unitOfAccountAmount)) {
+    const isBtcDenominatedUsdWalletAmount =
+      walletCurrency === WalletCurrency.Usd &&
+      unitOfAccountAmount.currency === WalletCurrency.Btc
+
+    const primaryAmount = convertMoneyAmount(unitOfAccountAmount, DisplayCurrency)
+
+    formattedPrimaryAmount = formatMoneyAmount({
+      moneyAmount: primaryAmount,
+    })
+
+    const secondaryAmount = getSecondaryAmountIfCurrencyIsDifferent({
+      primaryAmount,
+      walletAmount: convertMoneyAmount(unitOfAccountAmount, walletCurrency),
+      displayAmount: convertMoneyAmount(unitOfAccountAmount, DisplayCurrency),
+    })
+
+    formattedPrimaryAmount = formatMoneyAmount({
+      moneyAmount: primaryAmount,
+      isApproximate: isBtcDenominatedUsdWalletAmount && !secondaryAmount,
+    })
+
+    formattedSecondaryAmount =
+      secondaryAmount &&
+      formatMoneyAmount({
+        moneyAmount: secondaryAmount,
+        isApproximate:
+          isBtcDenominatedUsdWalletAmount &&
+          secondaryAmount.currency === WalletCurrency.Usd,
+      })
+  }
 
   return (
-    <Screen preset="scroll" style={styles.contentContainer}>
-      <View style={styles.Container}>
+    <Screen unsafe backgroundColor={colors.accent02}>
+      <View style={styles.container}>
         <SuccessIconAnimation>
-          <GaloyIcon name={"payment-success"} size={128} />
+          <GaloyIcon name={"send-success"} size={128} />
         </SuccessIconAnimation>
         <SuccessTextAnimation>
-          <Text {...testProps("Success Text")} style={styles.successText}>
+          <Text
+            {...testProps("Success Text")}
+            type="h01"
+            style={[styles.successText, { marginTop: 32 }]}
+          >
             {LL.SendBitcoinScreen.success()}
+          </Text>
+          <Text {...testProps("Success Text")} type="h03" style={styles.successText}>
+            {formattedPrimaryAmount}
+          </Text>
+          <Text
+            {...testProps("Success Text")}
+            type="h01"
+            style={[styles.successText, { color: "rgba(255,255,255,.7)" }]}
+          >
+            {formattedSecondaryAmount}
           </Text>
         </SuccessTextAnimation>
       </View>
+      <PrimaryBtn
+        label="Done"
+        onPress={() => navigation.popToTop()}
+        btnStyle={{
+          backgroundColor: "#fff",
+          marginHorizontal: 20,
+          marginBottom: bottom || 20,
+        }}
+        txtStyle={{ color: "#002118" }}
+      />
       <SuggestionModal
         navigation={navigation}
         showSuggestionModal={showSuggestionModal}
@@ -117,25 +184,16 @@ const SendBitcoinSuccessScreen = () => {
   )
 }
 
-const useStyles = makeStyles(({ colors }) => ({
-  contentContainer: {
-    flexGrow: 1,
-  },
-  successText: {
-    fontSize: 18,
-    textAlign: "center",
-    marginTop: 20,
-  },
-  Container: {
+const useStyles = makeStyles(() => ({
+  container: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
   },
-  view: {
-    marginHorizontal: 20,
-    backgroundColor: colors.white,
-    padding: 20,
-    borderRadius: 20,
+  successText: {
+    textAlign: "center",
+    color: "#fff",
+    marginBottom: 8,
   },
 }))
 
