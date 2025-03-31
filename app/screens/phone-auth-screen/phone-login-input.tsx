40f73f0a776f236a6a74c4c464c538e5895c61a8
nodirbek75 <nodirbek7077@gmail.com>
2025-03-31 20:15:42 +0500

    redesign and clean up phone authentication

--- app/screens/phone-auth-screen/phone-login-input.tsx
+++ app/screens/phone-auth-screen/phone-login-input.tsx
@@ -1,8 +1,8 @@
-import { useNavigation } from "@react-navigation/native"
-import { StackNavigationProp, StackScreenProps } from "@react-navigation/stack"
-import * as React from "react"
-import { useEffect } from "react"
+import React, { useEffect } from "react"
 import { ActivityIndicator, View } from "react-native"
+import { StackScreenProps } from "@react-navigation/stack"
+import { TouchableOpacity } from "react-native-gesture-handler"
+import { makeStyles, useTheme, Text, Input } from "@rneui/themed"
 import CountryPicker, {
   CountryCode,
   DARK_THEME,
@@ -13,104 +13,34 @@ import {
   CountryCode as PhoneNumberCountryCode,
   getCountryCallingCode,
 } from "libphonenumber-js/mobile"
-import { ContactSupportButton } from "@app/components/contact-support-button/contact-support-button"
 import { useI18nContext } from "@app/i18n/i18n-react"
-import { makeStyles, useTheme, Text, Input } from "@rneui/themed"
+
+// components
+import { ContactSupportButton } from "@app/components/contact-support-button/contact-support-button"
+import { GaloyErrorBox } from "@app/components/atomic/galoy-error-box"
+import { PrimaryBtn } from "@app/components/buttons"
 import { Screen } from "../../components/screen"
-import type { PhoneValidationStackParamList } from "../../navigation/stack-param-lists"
+
+// utils
 import {
   ErrorType,
   RequestPhoneCodeStatus,
   useRequestPhoneCodeLogin,
 } from "./request-phone-code-login"
-import { GaloyPrimaryButton } from "@app/components/atomic/galoy-primary-button"
-import { GaloySecondaryButton } from "@app/components/atomic/galoy-secondary-button"
-import { GaloyErrorBox } from "@app/components/atomic/galoy-error-box"
+
+// types
 import { PhoneCodeChannelType } from "@app/graphql/generated"
-import { TouchableOpacity } from "react-native-gesture-handler"
-console.log("PhoneLoginInitiateScreen")
+import { PhoneValidationStackParamList } from "@app/navigation/stack-param-lists"
+
 const DEFAULT_COUNTRY_CODE = "SV"
 const PLACEHOLDER_PHONE_NUMBER = "123-456-7890"
 
-const useStyles = makeStyles(({ colors }) => ({
-  screenStyle: {
-    padding: 20,
-    flexGrow: 1,
-  },
-  buttonsContainer: {
-    flex: 1,
-    justifyContent: "flex-end",
-  },
-
-  inputContainer: {
-    marginBottom: 20,
-    flexDirection: "row",
-    alignItems: "stretch",
-    minHeight: 48,
-  },
-  textContainer: {
-    marginBottom: 20,
-  },
-  viewWrapper: { flex: 1 },
-
-  activityIndicator: { marginTop: 12 },
-
-  keyboardContainer: {
-    paddingHorizontal: 10,
-  },
-
-  codeTextStyle: {},
-  countryPickerButtonStyle: {
-    minWidth: 110,
-    borderColor: colors.primary5,
-    borderWidth: 2,
-    borderRadius: 8,
-    paddingHorizontal: 10,
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-    flex: 1,
-  },
-  inputComponentContainerStyle: {
-    flex: 1,
-    marginLeft: 20,
-    paddingLeft: 0,
-    paddingRight: 0,
-  },
-  inputContainerStyle: {
-    flex: 1,
-    borderWidth: 2,
-    borderBottomWidth: 2,
-    paddingHorizontal: 10,
-    borderColor: colors.primary5,
-    borderRadius: 8,
-  },
-  errorContainer: {
-    marginBottom: 20,
-  },
-  whatsAppButton: {
-    marginBottom: 20,
-  },
-  contactSupportButton: {
-    marginTop: 10,
-  },
-
-  loadingView: { flex: 1, justifyContent: "center", alignItems: "center" },
-}))
-
 type Props = StackScreenProps<PhoneValidationStackParamList, "phoneLoginInitiate">
 
-export const PhoneLoginInitiateScreen: React.FC<Props> = ({ route }) => {
+export const PhoneLoginInitiateScreen: React.FC<Props> = ({ navigation, route }) => {
   const styles = useStyles()
-
-  const navigation =
-    useNavigation<
-      StackNavigationProp<PhoneValidationStackParamList, "phoneLoginInitiate">
-    >()
-
-  const {
-    theme: { colors, mode: themeMode },
-  } = useTheme()
+  const { colors, mode: themeMode } = useTheme().theme
+  const { LL } = useI18nContext()
 
   const {
     submitPhoneNumber,
@@ -129,8 +59,6 @@ export const PhoneLoginInitiateScreen: React.FC<Props> = ({ route }) => {
     loadingSupportedCountries,
   } = useRequestPhoneCodeLogin()
 
-  const { LL } = useI18nContext()
-
   useEffect(() => {
     if (status === RequestPhoneCodeStatus.SuccessRequestingCode) {
       setStatus(RequestPhoneCodeStatus.InputtingPhoneNumber)
@@ -141,16 +69,6 @@ export const PhoneLoginInitiateScreen: React.FC<Props> = ({ route }) => {
       })
     }
   }, [status, phoneCodeChannel, validatedPhoneNumber, navigation, setStatus])
-
-  if (status === RequestPhoneCodeStatus.LoadingCountryCode || loadingSupportedCountries) {
-    return (
-      <Screen>
-        <View style={styles.loadingView}>
-          <ActivityIndicator size="large" color={colors.primary} />
-        </View>
-      </Screen>
-    )
-  }
 
   let errorMessage: string | undefined
   if (error) {
@@ -176,44 +94,12 @@ export const PhoneLoginInitiateScreen: React.FC<Props> = ({ route }) => {
     errorMessage = LL.PhoneLoginInitiateScreen.errorUnsupportedCountry()
   }
 
-  let PrimaryButton = undefined
-  let SecondaryButton = undefined
-  switch (true) {
-    case isSmsSupported && isWhatsAppSupported:
-      PrimaryButton = (
-        <GaloyPrimaryButton
-          title={LL.PhoneLoginInitiateScreen.sms()}
-          loading={captchaLoading && phoneCodeChannel === PhoneCodeChannelType.Sms}
-          onPress={() => submitPhoneNumber(PhoneCodeChannelType.Sms)}
-        />
-      )
-      SecondaryButton = (
-        <GaloySecondaryButton
-          title={LL.PhoneLoginInitiateScreen.whatsapp()}
-          containerStyle={styles.whatsAppButton}
-          loading={captchaLoading && phoneCodeChannel === PhoneCodeChannelType.Whatsapp}
-          onPress={() => submitPhoneNumber(PhoneCodeChannelType.Whatsapp)}
-        />
-      )
-      break
-    case isSmsSupported && !isWhatsAppSupported:
-      PrimaryButton = (
-        <GaloyPrimaryButton
-          title={LL.PhoneLoginInitiateScreen.sms()}
-          loading={captchaLoading && phoneCodeChannel === PhoneCodeChannelType.Sms}
-          onPress={() => submitPhoneNumber(PhoneCodeChannelType.Sms)}
-        />
-      )
-      break
-    case !isSmsSupported && isWhatsAppSupported:
-      PrimaryButton = (
-        <GaloyPrimaryButton
-          title={LL.PhoneLoginInitiateScreen.whatsapp()}
-          loading={captchaLoading && phoneCodeChannel === PhoneCodeChannelType.Whatsapp}
-          onPress={() => submitPhoneNumber(PhoneCodeChannelType.Whatsapp)}
-        />
-      )
-      break
+  if (status === RequestPhoneCodeStatus.LoadingCountryCode || loadingSupportedCountries) {
+    return (
+      <Screen style={styles.loadingView}>
+        <ActivityIndicator size="large" color={colors.primary} />
+      </Screen>
+    )
   }
 
   return (
@@ -224,10 +110,9 @@ export const PhoneLoginInitiateScreen: React.FC<Props> = ({ route }) => {
       keyboardShouldPersistTaps="handled"
     >
       <View style={styles.viewWrapper}>
-        <View style={styles.textContainer}>
-          <Text type={"p1"}>{LL.PhoneLoginInitiateScreen.header()}</Text>
-        </View>
-
+        <Text type={"p1"} style={styles.header}>
+          {LL.PhoneLoginInitiateScreen.header()}
+        </Text>
         <View style={styles.inputContainer}>
           <CountryPicker
             theme={themeMode === "dark" ? DARK_THEME : DEFAULT_THEME}
@@ -271,17 +156,79 @@ export const PhoneLoginInitiateScreen: React.FC<Props> = ({ route }) => {
           />
         </View>
         {errorMessage && (
-          <View style={styles.errorContainer}>
+          <>
             <GaloyErrorBox errorMessage={errorMessage} />
             <ContactSupportButton containerStyle={styles.contactSupportButton} />
-          </View>
+          </>
         )}
-
-        <View style={styles.buttonsContainer}>
-          {SecondaryButton}
-          {PrimaryButton}
-        </View>
       </View>
+      {isSmsSupported && (
+        <PrimaryBtn
+          label={LL.PhoneLoginInitiateScreen.sms()}
+          loading={captchaLoading && phoneCodeChannel === PhoneCodeChannelType.Sms}
+          onPress={() => submitPhoneNumber(PhoneCodeChannelType.Sms)}
+          btnStyle={isWhatsAppSupported ? { marginBottom: 10 } : {}}
+        />
+      )}
+      {isWhatsAppSupported && (
+        <PrimaryBtn
+          type={isSmsSupported ? "outline" : "solid"}
+          label={LL.PhoneLoginInitiateScreen.whatsapp()}
+          loading={captchaLoading && phoneCodeChannel === PhoneCodeChannelType.Whatsapp}
+          onPress={() => submitPhoneNumber(PhoneCodeChannelType.Whatsapp)}
+        />
+      )}
     </Screen>
   )
 }
+
+const useStyles = makeStyles(({ colors }) => ({
+  screenStyle: {
+    padding: 20,
+    flexGrow: 1,
+  },
+  inputContainer: {
+    minHeight: 48,
+    flexDirection: "row",
+    marginBottom: 20,
+  },
+  header: {
+    marginBottom: 20,
+  },
+  viewWrapper: {
+    flex: 1,
+  },
+  countryPickerButtonStyle: {
+    minWidth: 110,
+    borderColor: colors.border02,
+    borderWidth: 1,
+    borderRadius: 10,
+    paddingHorizontal: 10,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    flex: 1,
+  },
+  inputComponentContainerStyle: {
+    flex: 1,
+    marginLeft: 20,
+    paddingLeft: 0,
+    paddingRight: 0,
+  },
+  inputContainerStyle: {
+    flex: 1,
+    borderWidth: 1,
+    paddingHorizontal: 10,
+    borderColor: colors.border02,
+    borderRadius: 10,
+  },
+  contactSupportButton: {
+    marginTop: 10,
+    backgroundColor: colors.button01,
+  },
+  loadingView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+}))
