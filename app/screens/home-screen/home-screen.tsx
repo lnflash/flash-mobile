e37ba0bd6dc3049b3a7e2f51ccb83674205ca5b6
nodirbek75 <nodirbek7077@gmail.com>
2025-04-04 11:31:00 +0500

    remove loading indicator on the home screen

--- app/screens/home-screen/home-screen.tsx
+++ app/screens/home-screen/home-screen.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useEffect, useState } from "react"
-import { ActivityIndicator, RefreshControl, ScrollView } from "react-native"
+import { RefreshControl, ScrollView } from "react-native"
 import { useTheme } from "@rneui/themed"
 
 // components
@@ -121,22 +121,12 @@ export const HomeScreen: React.FC = () => {
           setModalVisible={setModalVisible}
           setDefaultAccountModalVisible={setDefaultAccountModalVisible}
         />
-        {loadingAuthed ? (
-          <ActivityIndicator
-            size="large"
-            color={colors.primary}
-            style={{ marginTop: 50 }}
-          />
-        ) : (
-          <>
-            <QuickStart />
-            <Transactions
-              refreshTriggered={refreshTriggered}
-              loadingAuthed={loadingAuthed}
-              transactionsEdges={transactions as TransactionEdge[]}
-            />
-          </>
-        )}
+        <QuickStart />
+        <Transactions
+          refreshTriggered={refreshTriggered}
+          loadingAuthed={loadingAuthed}
+          transactionsEdges={transactions as TransactionEdge[]}
+        />
       </ScrollView>
       <SetDefaultAccountModal
         isVisible={defaultAccountModalVisible}
