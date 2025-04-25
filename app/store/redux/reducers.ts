426611b87cbb8a66b769929fe01310d142ac640e
nodirbek75 <nodirbek7077@gmail.com>
2025-04-25 11:02:44 +0500

    implemented businessAccountSlice to handle business account states globally

--- app/store/redux/reducers.ts
+++ app/store/redux/reducers.ts
@@ -2,7 +2,9 @@ import { combineReducers } from "@reduxjs/toolkit"
 
 // slices
 import userSlice from "./slices/userSlice"
+import businessAccountSlice from "./slices/businessAccountSlice"
 
 export default combineReducers({
   user: userSlice,
+  businessAccount: businessAccountSlice,
 })
