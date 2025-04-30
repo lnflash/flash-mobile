6f80f4f3822c876972d66f06e6c17b500efccab8
nodirbek75 <nodirbek7077@gmail.com>
2025-04-30 13:52:50 +0500

    add a input validation in BankInformation  screen

--- app/screens/account-upgrade-flow/BankInformation.tsx
+++ app/screens/account-upgrade-flow/BankInformation.tsx
@@ -1,6 +1,8 @@
-import React from "react"
+import React, { useState } from "react"
 import { ScrollView } from "react-native"
 import { makeStyles } from "@rneui/themed"
+import { StackScreenProps } from "@react-navigation/stack"
+import { RootStackParamList } from "@app/navigation/stack-param-lists"
 
 // components
 import {
@@ -31,14 +33,51 @@ const currencies = [
   { label: "XCG - Caribbean Guilder", value: "xcg" },
 ]
 
-const BankInformation = () => {
+type Props = StackScreenProps<RootStackParamList, "BankInformation">
+
+const BankInformation: React.FC<Props> = ({ navigation }) => {
+  const dispatch = useAppDispatch()
   const styles = useStyles()
 
-  const dispatch = useAppDispatch()
-  const { bankName, bankBranch, accountType, currency, accountNumber, document } =
+  const [nameErr, setNameErr] = useState<string>()
+  const [branchErr, setBranchErr] = useState<string>()
+  const [accountTypeErr, setAccountTypeErr] = useState<string>()
+  const [currencyErr, setCurrencyErr] = useState<string>()
+  const [accountNumErr, setAccountNumErr] = useState<string>()
+  const [idDocumentErr, setIdDocumentErr] = useState<string>()
+  const { bankName, bankBranch, accountType, currency, accountNumber, idDocument } =
     useAppSelector((state) => state.accountUpgrade.bankInfo)
 
-  const onPressNext = () => {}
+  const onPressNext = () => {
+    let hasError = false
+    if (bankName.length < 2) {
+      setNameErr("Bank name is required")
+      hasError = true
+    }
+    if (bankBranch.length < 2) {
+      setBranchErr("Branch is required")
+      hasError = true
+    }
+    if (!accountType) {
+      setAccountTypeErr("Account type is required")
+      hasError = true
+    }
+    if (!currency) {
+      setCurrencyErr("Currency is required")
+      hasError = true
+    }
+    if (accountNumber.length < 4) {
+      setAccountNumErr("Account number is required")
+      hasError = true
+    }
+    if (!idDocument) {
+      setIdDocumentErr("You must upload an ID document before proceeding")
+      hasError = true
+    }
+    if (!hasError) {
+      navigation.navigate("AccountUpgradeSuccess")
+    }
+  }
 
   return (
     <Screen>
@@ -47,35 +86,55 @@ const BankInformation = () => {
           label="Bank Name"
           placeholder="Enter your bank name"
           value={bankName}
-          onChangeText={(val) => dispatch(setBankInfo({ bankName: val }))}
+          errorMsg={nameErr}
+          onChangeText={(val) => {
+            setNameErr(undefined)
+            dispatch(setBankInfo({ bankName: val }))
+          }}
         />
         <InputField
           label="Bank Branch"
           placeholder="Enter your bank branch"
           value={bankBranch}
-          onChangeText={(val) => dispatch(setBankInfo({ bankBranch: val }))}
+          errorMsg={branchErr}
+          onChangeText={(val) => {
+            setBranchErr(undefined)
+            dispatch(setBankInfo({ bankBranch: val }))
+          }}
         />
         <DropDownField
           label="Account Type"
           placeholder={"Select account type"}
           data={accountTypes}
           value={accountType}
-          onChange={(val) => dispatch(setBankInfo({ accountType: val }))}
+          errorMsg={accountTypeErr}
+          onChange={(val) => {
+            setAccountTypeErr(undefined)
+            dispatch(setBankInfo({ accountType: val }))
+          }}
         />
         <DropDownField
           label="Currency"
           placeholder="Select currency"
           data={currencies}
           value={currency}
-          onChange={(val) => dispatch(setBankInfo({ currency: val }))}
+          errorMsg={currencyErr}
+          onChange={(val) => {
+            setCurrencyErr(undefined)
+            dispatch(setBankInfo({ currency: val }))
+          }}
         />
         <InputField
           label="Account Number"
           placeholder="Enter your account number"
           value={accountNumber}
-          onChangeText={(val) => dispatch(setBankInfo({ accountNumber: val }))}
+          errorMsg={accountNumErr}
+          onChangeText={(val) => {
+            setAccountNumErr(undefined)
+            dispatch(setBankInfo({ accountNumber: val }))
+          }}
         />
-        <PhotoUploadField label="Upload ID Document" />
+        <PhotoUploadField label="Upload ID Document" errorMsg={idDocumentErr} />
       </ScrollView>
       <PrimaryBtn label="Next" btnStyle={styles.btn} onPress={onPressNext} />
     </Screen>
