43b7dff41454b30b7dfda2aafb8de42088d1e29d
nodirbek75 <nodirbek7077@gmail.com>
2025-05-20 11:54:24 +0500

    implement cleanUp method in useLogout hook to clean up all cached states

--- app/hooks/use-logout.ts
+++ app/hooks/use-logout.ts
@@ -1,27 +1,22 @@
 import AsyncStorage from "@react-native-async-storage/async-storage"
-import crashlytics from "@react-native-firebase/crashlytics"
 import messaging from "@react-native-firebase/messaging"
-
-// utils
-import KeyStoreWrapper from "../utils/storage/secureStorage"
-import { logLogout } from "@app/utils/analytics"
 
 // store
 import { resetUserSlice } from "@app/store/redux/slices/userSlice"
 
 // hooks
 import { useApolloClient } from "@apollo/client"
-import { useCallback } from "react"
 import { useUserLogoutMutation } from "@app/graphql/generated"
 import { usePersistentStateContext } from "@app/store/persistent-state"
 import { useAppDispatch } from "@app/store/redux"
-
-import { SCHEMA_VERSION_KEY } from "@app/config"
 import { useFlashcard } from "./useFlashcard"
+
+// utils
+import KeyStoreWrapper from "../utils/storage/secureStorage"
+import { SCHEMA_VERSION_KEY } from "@app/config"
 
 const useLogout = () => {
   const client = useApolloClient()
-
   const dispatch = useAppDispatch()
   const { resetState } = usePersistentStateContext()
   const { resetFlashcard } = useFlashcard()
@@ -30,46 +25,38 @@ const useLogout = () => {
     fetchPolicy: "no-cache",
   })
 
-  const logout = useCallback(
-    async (stateToDefault = true): Promise<void> => {
-      try {
-        const deviceToken = await messaging().getToken()
+  const logout = async (stateToDefault = true) => {
+    try {
+      const deviceToken = await messaging().getToken()
+      userLogoutMutation({ variables: { input: { deviceToken } } })
+        .then(async (res) => {
+          console.log("USER LOGOUT MUTATION RES: ", res.data?.userLogout)
+          if (res.data?.userLogout.success) {
+            cleanUp()
+          }
+        })
+        .catch((err) => {
+          console.log("USER LOGOUT MUTATION ERR: ", err)
+        })
+    } catch (err: unknown) {
+      console.log("USER LOGOUT MUTATION ERR CATCH: ", err)
+    }
+  }
 
-        await client.cache.reset()
-        await AsyncStorage.multiRemove([SCHEMA_VERSION_KEY])
-        await KeyStoreWrapper.removeIsBiometricsEnabled()
-        await KeyStoreWrapper.removePin()
-        await KeyStoreWrapper.removePinAttempts()
-        dispatch(resetUserSlice())
-
-        logLogout()
-        if (stateToDefault) {
-          resetState()
-          resetFlashcard()
-        }
-
-        await Promise.race([
-          userLogoutMutation({ variables: { input: { deviceToken } } }),
-          // Create a promise that rejects after 2 seconds
-          // this is handy for the case where the server is down, or in dev mode
-          new Promise((_, reject) => {
-            setTimeout(() => {
-              reject(new Error("Logout mutation timeout"))
-            }, 2000)
-          }),
-        ])
-      } catch (err: unknown) {
-        if (err instanceof Error) {
-          crashlytics().recordError(err)
-          console.debug({ err }, `error logout`)
-        }
-      }
-    },
-    [resetState, client],
-  )
+  const cleanUp = async () => {
+    await client.cache.reset()
+    await AsyncStorage.multiRemove([SCHEMA_VERSION_KEY])
+    await KeyStoreWrapper.removeIsBiometricsEnabled()
+    await KeyStoreWrapper.removePin()
+    await KeyStoreWrapper.removePinAttempts()
+    dispatch(resetUserSlice())
+    resetState()
+    resetFlashcard()
+  }
 
   return {
     logout,
+    cleanUp,
   }
 }
 
