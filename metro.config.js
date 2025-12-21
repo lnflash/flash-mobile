fdc0d7741d799168f94e25a61c62b08078fe9922
Nodirbek Jamoldinov <nodirbek7077@gmail.com>
2025-12-21 14:45:24 +0500

    upgrade react-native to 0.74.7

--- metro.config.js
+++ metro.config.js
@@ -3,14 +3,30 @@ const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config")
 
 const defaultConfig = getDefaultConfig(__dirname)
 
+// Node.js core module polyfills for React Native 0.74+
+const nodeLibs = require("node-libs-react-native")
+
 module.exports = mergeConfig(defaultConfig, {
   transformer: {
     ...defaultConfig.transformer,
     babelTransformerPath: require.resolve("react-native-svg-transformer"),
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
   },
   resolver: {
     ...defaultConfig.resolver,
     assetExts: defaultConfig.resolver.assetExts.filter((ext) => ext !== "svg"),
     sourceExts: [...defaultConfig.resolver.sourceExts, "svg", "cjs", "json"],
+    extraNodeModules: {
+      ...nodeLibs,
+      crypto: require.resolve("crypto-browserify"),
+      stream: require.resolve("readable-stream"),
+      buffer: require.resolve("buffer"),
+      vm: require.resolve("vm-browserify"),
+    },
   },
 })
