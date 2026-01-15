587146bbadc056c3432511434bbea2a14aed17b7
Abhay <toabhayraizada@gmail.com>
2026-01-15 18:35:48 +0530

    Fetch Nostr Events via Central Singleton

--- app/screens/chat/NIP17Chat.tsx
+++ app/screens/chat/NIP17Chat.tsx
@@ -14,11 +14,10 @@ import { FlatList } from "react-native-gesture-handler"
 import Icon from "react-native-vector-icons/Ionicons"
 
 import { Screen } from "../../components/screen"
-import { bytesToHex } from "@noble/hashes/utils"
 import { testProps } from "../../utils/testProps"
 
 import { useI18nContext } from "@app/i18n/i18n-react"
-import { getPublicKey, nip19 } from "nostr-tools"
+import { nip19 } from "nostr-tools"
 import { convertRumorsToGroups, fetchSecretFromLocalStorage } from "@app/utils/nostr"
 import { useStyles } from "./style"
 import { HistoryListItem } from "./historyListItem"
@@ -64,7 +63,6 @@ export const NIP17Chat: React.FC = () => {
 
   const [initialized, setInitialized] = useState(false)
   const [searchedUsers, setSearchedUsers] = useState<Chat[]>([])
-  const [privateKey, setPrivateKey] = useState<Uint8Array>()
   const [showImportModal, setShowImportModal] = useState<boolean>(false)
   const [skipMismatchCheck, setskipMismatchCheck] = useState<boolean>(false)
   const { LL } = useI18nContext()
@@ -87,14 +85,13 @@ export const NIP17Chat: React.FC = () => {
         return
       }
 
-      const secret = nip19.decode(secretKeyString).data as Uint8Array
-      setPrivateKey(secret)
-
       const accountNpub = dataAuthed?.me?.npub
-      const storedNpub = nip19.npubEncode(getPublicKey(secret))
-      if (!skipMismatchCheck && accountNpub && storedNpub !== accountNpub) {
-        console.log("Account Info mismatch", accountNpub, storedNpub)
-        setShowImportModal(true)
+      if (userPublicKey) {
+        const storedNpub = nip19.npubEncode(userPublicKey)
+        if (!skipMismatchCheck && accountNpub && storedNpub !== accountNpub) {
+          console.log("Account Info mismatch", accountNpub, storedNpub)
+          setShowImportModal(true)
+        }
       }
 
       if (!initialized) {
@@ -104,7 +101,7 @@ export const NIP17Chat: React.FC = () => {
     }
 
     initialize()
-  }, [dataAuthed, initialized, skipMismatchCheck])
+  }, [dataAuthed, initialized, skipMismatchCheck, userPublicKey])
 
   // ------------------------
   // Focus effect for secret key / import modal check
@@ -119,11 +116,12 @@ export const NIP17Chat: React.FC = () => {
           setShowImportModal(true)
           return
         }
-        const secret = nip19.decode(secretKeyString).data as Uint8Array
-        const accountNpub = dataAuthed?.me?.npub
-        const storedNpub = nip19.npubEncode(getPublicKey(secret))
-        if (!skipMismatchCheck && accountNpub && storedNpub !== accountNpub) {
-          setShowImportModal(true)
+        if (userPublicKey) {
+          const accountNpub = dataAuthed?.me?.npub
+          const storedNpub = nip19.npubEncode(userPublicKey)
+          if (!skipMismatchCheck && accountNpub && storedNpub !== accountNpub) {
+            setShowImportModal(true)
+          }
         }
       }
 
@@ -135,7 +133,14 @@ export const NIP17Chat: React.FC = () => {
       return () => {
         isMounted = false
       }
-    }, [setSearchedUsers, dataAuthed, isAuthed, skipMismatchCheck, initialized]),
+    }, [
+      setSearchedUsers,
+      dataAuthed,
+      isAuthed,
+      skipMismatchCheck,
+      initialized,
+      userPublicKey,
+    ]),
   )
 
   // ------------------------
@@ -168,8 +173,7 @@ export const NIP17Chat: React.FC = () => {
     return (lastBRumor?.created_at || 0) - (lastARumor?.created_at || 0)
   })
 
-  const currentUserPubKey = privateKey ? getPublicKey(privateKey) : null
-  const userProfile = currentUserPubKey ? profileMap?.get(currentUserPubKey) : null
+  const userProfile = userPublicKey ? profileMap?.get(userPublicKey) : null
 
   // ------------------------
   // Render
@@ -177,7 +181,7 @@ export const NIP17Chat: React.FC = () => {
   return (
     <Screen style={{ flex: 1 }}>
       <StatusBar translucent backgroundColor="transparent" />
-      {privateKey && !showImportModal ? (
+      {userPublicKey && !showImportModal ? (
         <View style={{ flex: 1, paddingTop: statusBarHeight }}>
           <Tab.Navigator
             screenOptions={({ route }) => {
@@ -207,9 +211,7 @@ export const NIP17Chat: React.FC = () => {
                       contentContainerStyle={styles.listContainer}
                       data={searchedUsers}
                       ListEmptyComponent={ListEmptyContent}
-                      renderItem={({ item }) => (
-                        <SearchListItem item={item} userPrivateKey={privateKey!} />
-                      )}
+                      renderItem={({ item }) => <SearchListItem item={item} />}
                       keyExtractor={(item) => item.id}
                     />
                   ) : (
@@ -219,8 +221,7 @@ export const NIP17Chat: React.FC = () => {
                         <Text style={styles.usernameText}>
                           signed in as:{" "}
                           <Text style={{ color: colors.primary, fontWeight: "bold" }}>
-                            {userData?.username ||
-                              nip19.npubEncode(getPublicKey(privateKey))}
+                            {userData?.username || nip19.npubEncode(userPublicKey)}
                           </Text>
                         </Text>
                       </View>
@@ -291,11 +292,7 @@ export const NIP17Chat: React.FC = () => {
                         ListEmptyComponent={ListEmptyContent}
                         scrollEnabled={true}
                         renderItem={({ item }) => (
-                          <HistoryListItem
-                            item={item}
-                            userPrivateKey={privateKey!}
-                            groups={groups}
-                          />
+                          <HistoryListItem item={item} groups={groups} />
                         )}
                         keyExtractor={(item) => item}
                       />
@@ -309,15 +306,14 @@ export const NIP17Chat: React.FC = () => {
               name={`Profile: ${userProfile?.name}`}
               component={ContactDetailsScreen}
               initialParams={{
-                contactPubkey: getPublicKey(privateKey),
-                userPrivateKey: bytesToHex(privateKey),
+                contactPubkey: userPublicKey,
               }}
             />
 
             <Tab.Screen name="Contacts">
               {() => (
                 <View style={{ height: "100%" }}>
-                  <Contacts userPrivateKey={bytesToHex(privateKey)} />
+                  <Contacts />
                 </View>
               )}
             </Tab.Screen>
