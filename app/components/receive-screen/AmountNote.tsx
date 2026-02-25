dc1a9c8ddf0a8430f673f67c089d91cfe56245d5
Nodirbek Jamoldinov <nodirbek7077@gmail.com>
2026-02-26 00:33:36 +0500

    remove lightning and onChain limits to set minAmount and MaxAmount since spark doesn't expose limits

--- app/components/receive-screen/AmountNote.tsx
+++ app/components/receive-screen/AmountNote.tsx
@@ -1,20 +1,10 @@
-import React, { useEffect, useState } from "react"
+import React from "react"
 import { View } from "react-native"
 import { makeStyles } from "@rneui/themed"
 
 // components
 import { AmountInput } from "../amount-input"
 import { NoteInput } from "../note-input"
-
-// types
-import { MoneyAmount, WalletOrDisplayCurrency } from "@app/types/amounts"
-import { Invoice } from "@app/screens/receive-bitcoin-screen/payment/index.types"
-
-// utils
-import {
-  fetchBreezLightningLimits,
-  fetchBreezOnChainLimits,
-} from "@app/utils/breez-sdk-liquid"
 
 type Props = {
   request: any
@@ -23,72 +13,30 @@ type Props = {
 const AmountNote: React.FC<Props> = ({ request }) => {
   const styles = useStyles()
 
-  const [minAmount, setMinAmount] = useState<MoneyAmount<WalletOrDisplayCurrency>>()
-  const [maxAmount, setMaxAmount] = useState<MoneyAmount<WalletOrDisplayCurrency>>()
-
-  useEffect(() => {
-    fetchMinMaxAmount()
-  }, [request.receivingWalletDescriptor.currency, request.type])
-
-  const fetchMinMaxAmount = async () => {
-    if (request.receivingWalletDescriptor.currency === "BTC") {
-      let limits
-      if (request.type === Invoice.Lightning) {
-        limits = await fetchBreezLightningLimits()
-      } else if (request.type === Invoice.OnChain) {
-        limits = await fetchBreezOnChainLimits()
-      }
-
-      setMinAmount({
-        amount: limits?.receive.minSat || 0,
-        currency: "BTC",
-        currencyCode: "SAT",
-      })
-      setMaxAmount({
-        amount: limits?.receive.maxSat || 0,
-        currency: "BTC",
-        currencyCode: "SAT",
-      })
-    } else {
-      setMinAmount({
-        amount: 1,
-        currency: "USD",
-        currencyCode: "USD",
-      })
-      setMaxAmount(undefined)
-    }
-  }
-
-  if (request.type !== "PayCode") {
-    return (
-      <View style={styles.container}>
-        <AmountInput
-          request={request}
-          unitOfAccountAmount={request.unitOfAccountAmount}
-          setAmount={request.setAmount}
-          canSetAmount={request.canSetAmount}
-          convertMoneyAmount={request.convertMoneyAmount}
-          walletCurrency={request.receivingWalletDescriptor.currency}
-          showValuesIfDisabled={false}
-          minAmount={minAmount}
-          maxAmount={maxAmount}
-          big={false}
-          newDesign={true}
-        />
-        <NoteInput
-          onBlur={request.setMemo}
-          onChangeText={request.setMemoChangeText}
-          value={request.memoChangeText || ""}
-          editable={request.canSetMemo}
-          style={{ marginTop: 10 }}
-          big={false}
-          newDesign={true}
-        />
-      </View>
-    )
-  } else {
-    return null
-  }
+  if (request.type === "PayCode") return null
+  return (
+    <View style={styles.container}>
+      <AmountInput
+        unitOfAccountAmount={request.unitOfAccountAmount}
+        setAmount={request.setAmount}
+        canSetAmount={request.canSetAmount}
+        convertMoneyAmount={request.convertMoneyAmount}
+        walletCurrency={request.receivingWalletDescriptor.currency}
+        showValuesIfDisabled={false}
+        big={false}
+        newDesign={true}
+      />
+      <NoteInput
+        onBlur={request.setMemo}
+        onChangeText={request.setMemoChangeText}
+        value={request.memoChangeText || ""}
+        editable={request.canSetMemo}
+        style={{ marginTop: 10 }}
+        big={false}
+        newDesign={true}
+      />
+    </View>
+  )
 }
 
 export default AmountNote
