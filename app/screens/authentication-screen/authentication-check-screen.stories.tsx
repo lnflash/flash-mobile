25a79c63b66e63303ec747287b7fcedc88741df5
Forge0x <forge@getflash.io>
2026-03-01 05:52:10 -0500

    fix(storybook): authentication-check-screen - add navigation mock for StackScreenProps, add Authed/Unauthed variants

--- app/screens/authentication-screen/authentication-check-screen.stories.tsx
+++ app/screens/authentication-screen/authentication-check-screen.stories.tsx
@@ -5,18 +5,54 @@ import mocks from "../../graphql/mocks"
 import { AuthenticationCheckScreen } from "./authentication-check-screen"
 import { Meta } from "@storybook/react"
 import { AuthenticationContextProvider } from "../../navigation/navigation-container-wrapper"
+import { IsAuthedContextProvider } from "../../graphql/is-authed-context"
+
+const mockNavigation = {
+  navigate: (name: string, params?: any) => console.log("navigate", name, params),
+  goBack: () => console.log("goBack"),
+  dispatch: () => {},
+  setOptions: () => {},
+  addListener: () => () => {},
+  removeListener: () => {},
+  isFocused: () => true,
+  canGoBack: () => false,
+  getParent: () => undefined,
+  getState: () => ({ index: 0, routes: [] }),
+  reset: () => {},
+  replace: () => {},
+} as any
+
+const mockRoute = {
+  key: "authenticationCheck",
+  name: "authenticationCheck" as const,
+  params: undefined,
+} as any
 
 export default {
   title: "Authentication Check Screen",
   component: AuthenticationCheckScreen,
 } as Meta<typeof AuthenticationCheckScreen>
 
-export const Default = () => (
+export const Authed = () => (
   <MockedProvider mocks={mocks} cache={createCache()}>
     <AuthenticationContextProvider
       value={{ isAppLocked: true, setAppUnlocked: () => {}, setAppLocked: () => {} }}
     >
-      <AuthenticationCheckScreen />
+      <IsAuthedContextProvider value={true}>
+        <AuthenticationCheckScreen navigation={mockNavigation} route={mockRoute} />
+      </IsAuthedContextProvider>
+    </AuthenticationContextProvider>
+  </MockedProvider>
+)
+
+export const Unauthed = () => (
+  <MockedProvider mocks={mocks} cache={createCache()}>
+    <AuthenticationContextProvider
+      value={{ isAppLocked: false, setAppUnlocked: () => {}, setAppLocked: () => {} }}
+    >
+      <IsAuthedContextProvider value={false}>
+        <AuthenticationCheckScreen navigation={mockNavigation} route={mockRoute} />
+      </IsAuthedContextProvider>
     </AuthenticationContextProvider>
   </MockedProvider>
 )
