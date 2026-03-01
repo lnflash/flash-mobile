59582d034677cb58a0d115c5fccd5f2d3c04e6e7
Forge0x <forge@getflash.io>
2026-03-01 05:43:15 -0500

    fix(storybook): home-screen - add Redux Provider and PersistentStateProvider

--- app/screens/home-screen/home-screen.stories.tsx
+++ app/screens/home-screen/home-screen.stories.tsx
@@ -1,10 +1,13 @@
 import React from "react"
+import { Provider } from "react-redux"
 import { HomeScreen } from "./home-screen"
 import { StoryScreen } from "../../../.storybook/views"
 import { Meta } from "@storybook/react"
 import { MockedProvider } from "@apollo/client/testing"
 import { createCache } from "../../graphql/cache"
 import { IsAuthedContextProvider } from "../../graphql/is-authed-context"
+import { PersistentStateProvider } from "../../store/persistent-state"
+import { store } from "../../store/redux"
 import mocks from "../../graphql/mocks"
 
 export default {
@@ -12,9 +15,13 @@ export default {
   component: HomeScreen,
   decorators: [
     (Story) => (
-      <MockedProvider mocks={mocks} cache={createCache()}>
-        <StoryScreen>{Story()}</StoryScreen>
-      </MockedProvider>
+      <Provider store={store}>
+        <PersistentStateProvider>
+          <MockedProvider mocks={mocks} cache={createCache()}>
+            <StoryScreen>{Story()}</StoryScreen>
+          </MockedProvider>
+        </PersistentStateProvider>
+      </Provider>
     ),
   ],
 } as Meta<typeof HomeScreen>
@@ -24,6 +31,7 @@ export const Unauthed = () => (
     <HomeScreen />
   </IsAuthedContextProvider>
 )
+
 export const Authed = () => (
   <IsAuthedContextProvider value={true}>
     <HomeScreen />
