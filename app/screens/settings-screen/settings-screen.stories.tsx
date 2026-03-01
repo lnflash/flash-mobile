96bd0a672f94705530d603536df05ab6e790a439
Forge0x <forge@getflash.io>
2026-03-01 05:43:46 -0500

    fix(storybook): settings-screen - update mock shape (add totpEnabled/email/wallets, remove displayCurrency)

--- app/screens/settings-screen/settings-screen.stories.tsx
+++ app/screens/settings-screen/settings-screen.stories.tsx
@@ -1,4 +1,5 @@
 import * as React from "react"
+import { Provider } from "react-redux"
 import { StoryScreen } from "../../../.storybook/views"
 import { SettingsScreen } from "./settings-screen"
 import { Meta } from "@storybook/react"
@@ -7,66 +8,67 @@ import { createCache } from "../../graphql/cache"
 import { SettingsScreenDocument } from "../../graphql/generated"
 import mocks from "../../graphql/mocks"
 import { MockedProvider } from "@apollo/client/testing"
+import { PersistentStateProvider } from "../../store/persistent-state"
+import { store } from "../../store/redux"
 
-const mocksWithUsername = [
-  ...mocks,
+const wallets = [
   {
-    request: {
-      query: SettingsScreenDocument,
-    },
-    result: {
-      data: {
-        me: {
-          id: "70df9822-efe0-419c-b864-c9efa99872ea",
-          phone: "+50365055539",
-          username: "test1",
-          language: "",
-          defaultAccount: {
-            id: "84b26b88-89b0-5c6f-9d3d-fbead08f79d8",
-            displayCurrency: "EUR",
-            defaultWalletId: "84b26b88-89b0-5c6f-9d3d-fbead08f79d8",
-            __typename: "ConsumerAccount",
-          },
-          __typename: "User",
-        },
-      },
-    },
+    id: "f79792e3-282b-45d4-85d5-7486d020def5",
+    balance: 88413,
+    walletCurrency: "BTC",
+    __typename: "BTCWallet",
+  },
+  {
+    id: "f091c102-6277-4cc6-8d81-87ebf6aaad1b",
+    balance: 158,
+    walletCurrency: "USD",
+    __typename: "UsdWallet",
   },
 ]
 
-const mocksNoUsername = [
-  {
-    request: {
-      query: SettingsScreenDocument,
-    },
-    result: {
-      data: {
-        me: {
-          id: "70df9822-efe0-419c-b864-c9efa99872ea",
-          phone: "+50365055539",
-          username: "",
-          language: "",
-          defaultAccount: {
-            id: "84b26b88-89b0-5c6f-9d3d-fbead08f79d8",
-            displayCurrency: "EUR",
-            defaultWalletId: "84b26b88-89b0-5c6f-9d3d-fbead08f79d8",
-            __typename: "ConsumerAccount",
-          },
-          __typename: "User",
+const makeSettingsMock = (username: string) => ({
+  request: { query: SettingsScreenDocument },
+  result: {
+    data: {
+      me: {
+        id: "70df9822-efe0-419c-b864-c9efa99872ea",
+        phone: "+50365055539",
+        username,
+        language: "",
+        totpEnabled: false,
+        defaultAccount: {
+          id: "84b26b88-89b0-5c6f-9d3d-fbead08f79d8",
+          defaultWalletId: "f79792e3-282b-45d4-85d5-7486d020def5",
+          wallets,
+          __typename: "ConsumerAccount",
         },
+        email: {
+          address: "test@getflash.io",
+          verified: true,
+          __typename: "Email",
+        },
+        __typename: "User",
       },
     },
   },
-]
+})
+
+const Decorators = (Story: React.FC) => (
+  <Provider store={store}>
+    <PersistentStateProvider>
+      <StoryScreen>{Story()}</StoryScreen>
+    </PersistentStateProvider>
+  </Provider>
+)
 
 export default {
   title: "Settings Screen",
   component: SettingsScreen,
-  decorators: [(Story) => <StoryScreen>{Story()}</StoryScreen>],
+  decorators: [Decorators],
 } as Meta<typeof SettingsScreen>
 
 export const NotLoggedIn = () => (
-  <MockedProvider cache={createCache()}>
+  <MockedProvider mocks={mocks} cache={createCache()}>
     <IsAuthedContextProvider value={false}>
       <SettingsScreen />
     </IsAuthedContextProvider>
@@ -74,7 +76,7 @@ export const NotLoggedIn = () => (
 )
 
 export const LoggedInNoUsername = () => (
-  <MockedProvider mocks={mocksNoUsername} cache={createCache()}>
+  <MockedProvider mocks={[...mocks, makeSettingsMock("")]} cache={createCache()}>
     <IsAuthedContextProvider value={true}>
       <SettingsScreen />
     </IsAuthedContextProvider>
@@ -82,7 +84,31 @@ export const LoggedInNoUsername = () => (
 )
 
 export const LoggedInWithUsername = () => (
-  <MockedProvider mocks={mocksWithUsername} cache={createCache()}>
+  <MockedProvider mocks={[...mocks, makeSettingsMock("test1")]} cache={createCache()}>
+    <IsAuthedContextProvider value={true}>
+      <SettingsScreen />
+    </IsAuthedContextProvider>
+  </MockedProvider>
+)
+
+export const LoggedInWithTOTP = () => (
+  <MockedProvider
+    mocks={[
+      ...mocks,
+      {
+        request: { query: SettingsScreenDocument },
+        result: {
+          data: {
+            me: {
+              ...makeSettingsMock("test1").result.data.me,
+              totpEnabled: true,
+            },
+          },
+        },
+      },
+    ]}
+    cache={createCache()}
+  >
     <IsAuthedContextProvider value={true}>
       <SettingsScreen />
     </IsAuthedContextProvider>
