d6b8fb2d9f054dfcfa8007fd3c68e012ef4e8178
nodirbek75 <nodirbek7077@gmail.com>
2026-05-19 12:49:51 +0500

    use getInternalWallets method to filter internal wallets on the send-bitcoin-details screen

--- app/screens/send-bitcoin-screen/send-bitcoin-details-screen.tsx
+++ app/screens/send-bitcoin-screen/send-bitcoin-details-screen.tsx
@@ -23,7 +23,7 @@ import {
   WalletCurrency,
 } from "@app/graphql/generated"
 import { decodeInvoiceString, Network as NetworkLibGaloy } from "@galoymoney/client"
-import { getUsdWallet } from "@app/graphql/wallets-utils"
+import { getInternalWallets, getUsdWallet } from "@app/graphql/wallets-utils"
 
 // hooks
 import { useIsAuthed } from "@app/graphql/is-authed-context"
@@ -382,7 +382,7 @@ const SendBitcoinDetailsScreen: React.FC<Props> = ({ navigation, route }) => {
       >
         <ChooseWallet
           usdWallet={usdWallet}
-          wallets={data?.me?.defaultAccount?.wallets as any[]}
+          wallets={getInternalWallets(data?.me?.defaultAccount?.wallets) as any[]}
           paymentDetail={paymentDetail}
           setPaymentDetail={setPaymentDetail}
         />
